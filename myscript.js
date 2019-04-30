eb15b00866eb21be3077e7437d1cd8209aa387f8
Cecilia E.T. Söderblom <ceciliasoderblom@Cecilias-MacBook-Pro-2.local>
2019-04-30 11:39:39 +0200

    add tekst fra JSON fil ind til SVG

--- myscript.js
+++ myscript.js
@@ -1,6 +1,52 @@
 window.addEventListener("DOMContentLoaded", loadSVG);
+window.addEventListener("DOMContentLoaded", loadJson);
 window.addEventListener("resize", calSize);
+let movieTemplate = document.querySelector("#movie_template");
 
+// LOAD JSON
+function loadJson() {
+  fetch("data.json")
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log("The jsonData is loaded");
+      console.log(jsonData);
+      //LOOP
+      jsonData.forEach(dataen => {
+        console.log(dataen);
+        // SET DESTINATIONEN SÅ DATA KOMMER IND I RETTE DIV
+        const dest = document.querySelector(
+          `[data-svgplaceholder=${dataen.id}]`
+        );
+        console.log(dest);
+
+        // VIS OG KLON PÅ NY VIS
+        let klon = movieTemplate.cloneNode(true).content;
+
+        const dataFields = klon.querySelectorAll("[data-field]");
+        dataFields.forEach(element => {
+          /* console.log(element); */
+          const prop = element.dataset.field;
+          /* console.log(prop); */
+          element.textContent = dataen[prop];
+          element.src = dataen[prop];
+        });
+
+        // VIS OG KLON PÅ GAMMEL VIS
+        /* 
+        klon.querySelector("h1").textContent = dataen.title;
+        klon.querySelector("[data-field='year']").textContent = dataen.year;
+        klon.querySelector("[data-field='director']").textContent =
+          dataen.director;
+        klon.querySelector("img").src = dataen.image;
+ */
+
+        // PUSH TIL DESTINATIONEN. SOM ALTID
+        dest.appendChild(klon);
+      });
+    });
+}
+
+// LOAD SVG
 function loadSVG() {
   console.log("Load SVG");
 
@@ -16,7 +62,7 @@ function loadSVG() {
       calSize();
     });
 }
-
+// UDREGN STØRRELSERNE OG SET HTML TEKSTEN SAMME STØRRELSE SOM SVG BOKSEN/ELEMENTET
 function calSize() {
   fitRectangle("#philosopher .HTML_placeholder", "#movie_1");
   fitRectangle("#chamber .HTML_placeholder", "#movie_2");
